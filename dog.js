3a50e5d492dd00c1f279fd98abd19c0f664052df
KTan <nguyenviettantan@gmail.com>
2020-09-02 18:27:19 +0700

    Add something scrap

--- dog.js
+++ dog.js
@@ -1,2 +1,13 @@
-function Dog(){
+chalk = require('chalk');
+
+function Dog(name){
+	this.name = name;
+	this.stomach =[];
 }
+Dog.prototype.eat = function(cat){
+	this.stomach.push(cat);
+}
+Dog.prototype.sayHi= function(){
+	console.log("I'm Dog. My name is " +chalk.blue(this.name))
+}
+module.exports = Dog;
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var Cat = require('./cat.js');
 var Mouse = require('./mouse.js');
-
+var Dog = require('./dog.js');
 var Mouse1 = new Mouse('Mickey');
 var Mouse2 = new Mouse('Jerry');
 var tom = new Cat();
-tom.eat(Mouse1);
-console.log(Cat);
+// tom.eat(Mouse1);
+// console.log(tom);
+// console.log(Mouse1);
+var husky = new Dog('Husky');
+console.log(husky);
+husky.sayHi();
